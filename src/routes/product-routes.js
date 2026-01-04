58a61048322a790e266ab0336880589301886969
Sky's light <preciousarts2478@gmail.com>
2026-01-04 05:14:27 -0800

    applied changes and fixed bugs for search product controllers

--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const isAuth = require("../middleware/auth-midware");
-const { createProducts, getProducts, getSingleProduct, updateProduct, deleteProduct, searchByitemTitle, searchByCategory } = require("../controller/product-controller");
+const { createProducts, searchByitemTitle, searchByCategory, getSingleProduct, getProducts, updateProduct, deleteProduct } = require("../controller/product-controller");
 
 const router = express.Router();
 
 router.post("/", isAuth, createProducts);
-router.get("/", getProducts);
-router.get("/:id", getSingleProduct);
-router.put("/:id", isAuth, updateProduct);
-router.delete("/:id", isAuth, deleteProduct);
 router.get("/search/title", searchByitemTitle);
 router.get("/search/category", searchByCategory);
+router.get("/:id", getSingleProduct);
+router.get("/", getProducts);
+router.put("/:id", isAuth, updateProduct);
+router.delete("/:id", isAuth, deleteProduct);
+
 
 
 module.exports = router;
